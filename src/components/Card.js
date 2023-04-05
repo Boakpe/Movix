3173e356d07a81dd06cbe61ecc8b71e78b5f6e36
Boakpe <breno.cpedroso@gmail.com>
2023-04-05 18:04:27 -0300

    Add card add movie functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,23 @@
 import { Link } from "react-router-dom";
+import usePost from "../hooks/usePost";
+import useFetch from "../hooks/useFetch";
+//import { useState } from "react";
 
 const Card = ({ id, title, description, image, api_id }) => {
+  const url = "http://localhost:3000/movies";
+  const { isLoading, error, doPost } = usePost(url);
+  const { data: movies } = useFetch(url);
+
+  const handleAddMovie = () => {
+    const newMovie = { title };
+    doPost(newMovie);
+  };
+
+  const was_added = () => {
+    return movies?.some((movie) => movie.title === title);
+  }
+  
+
   return (
     <div className="col-md-3 mb-3 d-flex align-items-stretch">
       <div className="card game-card ">
@@ -10,8 +27,26 @@ const Card = ({ id, title, description, image, api_id }) => {
             <strong>{title}</strong>
           </h5>
 
-          <p className="card-text text-secondary text-truncate">{description}</p>
-          <Link to={"/movies/" + id} state={{from: api_id}} className="btn btn-outline-primary mt-auto align-self-start">More info</Link>
+          <p className="card-text text-secondary text-truncate">
+            {description}
+          </p>
+          <Link
+            to={"/movies/" + id}
+            state={{ from: api_id }}
+            className="btn btn-outline-primary mt-auto align-self-start"
+          >
+            More info
+          </Link>
+          { !was_added() && movies  &&
+            <button
+            onClick={handleAddMovie}
+            className="btn btn-outline-success mt-2 align-self-start"
+          >
+            {isLoading ? "Loading..." : "Add Movie"}
+          </button>
+          }
+          
+          {error && <p className="text-danger mt-2">{error.message}</p>}
         </div>
       </div>
     </div>
